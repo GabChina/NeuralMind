176035b31562007e4f88b141a8fa5951336b07b1
Lucas Brandão <brandao.brandao@discente.ufg.br>
2022-11-23 15:34:23 -0300

    Added pandas2json function

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,25 @@
+import pandas as pd
+import json
+import numpy as np
 
+
+def pandas2json(df, fname: str):
+    """Convert pandas to json file
+
+    Args:
+        df (pd.DataFrame): Dataframe Object
+        fname (str): file name
+    """
+
+    texts = []
+    for i in range(len(df)):
+        text_dict = {
+            "text": df['text'].iloc[i],
+            "tags": df['tags'].iloc[i]
+        }
+        texts.append(text_dict)
+
+    with open(fname, 'w', encoding='utf8') as file:
+        for text in texts:
+            json.dump(text, file, ensure_ascii=False)
+            file.write('\n')
