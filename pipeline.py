3881163fa9996a9ab0e445962125022d89d86913
Lucas Brandão <brandao.brandao@discente.ufg.br>
2022-11-23 15:49:37 -0300

    Added requirements.txt and pipeline.py

---
+++ pipeline.py
@@ -0,0 +1,19 @@
+import os
+import json
+import numpy as np
+import tensorflow as tf
+import matplotlib
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer, TrainingArguments, Trainer
+from transformers import AutoModelForTokenClassification, DataCollatorForTokenClassification
+from datasets import load_metric
+
+
+class NM_Dataset:
+    pass
+
+class NM_Model:
+    class NM_Trainer:
+        pass
+    pass
