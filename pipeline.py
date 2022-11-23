fdd5345dcddf1dbf9b82d0985f2a597fa478e436
Lucas Brandão <brandao.brandao@discente.ufg.br>
2022-11-23 16:39:58 -0300

    Added load and save functions to NM_Dataset

--- pipeline.py
+++ pipeline.py
@@ -7,11 +7,20 @@ from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, TrainingArguments, Trainer
 from transformers import AutoModelForTokenClassification, DataCollatorForTokenClassification
-from datasets import load_metric
+from datasets import Dataset, load_dataset, load_metric
 
 
 class NM_Dataset:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def load_dataset(self, path: str):
+        """Loads a .hf file into a Dataset variable."""
+        self.dataset = Dataset.load_from_disk(path)
+
+    def save_dataset(self, path: str):
+        """Saves the dataset as a .hf file."""
+        self.dataset.save_to_disk(path)
 
 class NM_Model:
     class NM_Trainer:
